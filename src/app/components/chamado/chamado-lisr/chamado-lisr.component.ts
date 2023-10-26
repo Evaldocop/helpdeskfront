81b701694622d1d8cd26063155cca557f9be2544
evaldo.ferriera <evaldo.ferriera@fornecedores.goias.gov.br>
2023-10-26 10:02:47 -0300

    componente chamado criado

---
+++ src/app/components/chamado/chamado-lisr/chamado-lisr.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-chamado-lisr',
+  templateUrl: './chamado-lisr.component.html',
+  styleUrls: ['./chamado-lisr.component.css']
+})
+export class ChamadoLisrComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
